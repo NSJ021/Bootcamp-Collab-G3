95c1041a84a50c8571626c319721e41508cac215
Nathan Jones <104011128+NSJ021@users.noreply.github.com>
2023-07-13 18:52:01 +0100

    First upload of Group_3.py

---
+++ Group_3.py
@@ -0,0 +1,69 @@
+"""
+    --- Quiz App/Game ---
+    
+    --- Bootcmap Collaboration Project ---
+    
+    --- Group 3 ---
+    
+    Nathan Jones
+    Sunite Lauchande
+    Xavier Emmanuel Tibby
+    Muhammad Anwar
+    
+    ---------------------------------------
+    
+    --- Requirements ---
+    
+    4 Questions from one of the GMetric's Domains
+    Each Question to be multiple choice with 4 answers.
+    Code needs to be adaptable, so it can be added to the other groups code in time to create the full App/Game
+    
+    --- Ideas ---
+    
+    Use of functions, wrap our code in 1 callable function?
+    Use list and or dictionaries to store the Questions and their choices?
+    Utilise If statements to control the flow of question to question and if right or wrong?
+    
+"""
+
+
+
+#imports
+
+
+
+#functions
+
+
+
+
+#variables
+
+
+
+
+
+#main code block
+
+print("""
+      
+      
+=======================================================================        
+  ______             __                   ______                      
+ /      \           /  |                 /      \                     
+/$$$$$$  | __    __ $$/  ________       /$$$$$$  |  ______    ______  
+$$ |  $$ |/  |  /  |/  |/        |      $$ |__$$ | /      \  /      \ 
+$$ |  $$ |$$ |  $$ |$$ |$$$$$$$$/       $$    $$ |/$$$$$$  |/$$$$$$  |
+$$ |_ $$ |$$ |  $$ |$$ |  /  $$/        $$$$$$$$ |$$ |  $$ |$$ |  $$ |
+$$ / \$$ |$$ \__$$ |$$ | /$$$$/__       $$ |  $$ |$$ |__$$ |$$ |__$$ |
+$$ $$ $$< $$    $$/ $$ |/$$      |      $$ |  $$ |$$    $$/ $$    $$/ 
+ $$$$$$  | $$$$$$/  $$/ $$$$$$$$/       $$/   $$/ $$$$$$$/  $$$$$$$/  
+     $$$/                                         $$ |      $$ |      
+                                                  $$ |      $$ |      
+                                                  $$/       $$/                                                         
+=======================================================================
+
+
+""")
+
+
